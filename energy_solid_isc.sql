32b341cbb34b04581b298f5d29e99a3f958ede2c
peter279k <peter279k@gmail.com>
2019-09-24 11:05:52 +0800

    Initialize database and table schema data

---
+++ energy_solid_isc.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 2019 年 09 月 24 日 11:03
+-- 伺服器版本： 5.7.27-0ubuntu0.16.04.1
+-- PHP 版本： 7.3.9-1+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `energy_solid_isc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `ID` int(10) NOT NULL,
+  `account` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `accounts`
+--
+
+INSERT INTO `accounts` (`ID`, `account`, `password`) VALUES
+(1, 'itri', '$2y$10$9xbvZwOfbfMJSFMKgwyrZOyCdvAl7gCjONZRFvYQYVawX5CplDaa6'),
+(2, 'test', '$2y$10$9xbvZwOfbfMJSFMKgwyrZOyCdvAl7gCjONZRFvYQYVawX5CplDaa6');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `ID` int(11) NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expired` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- 資料表索引 `tokens`
+--
+ALTER TABLE `tokens`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- 在傾印的資料表使用自動增長(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動增長(AUTO_INCREMENT) `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動增長(AUTO_INCREMENT) `tokens`
+--
+ALTER TABLE `tokens`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
